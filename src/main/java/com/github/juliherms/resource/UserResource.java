a30729dc07871dcbef3a04bb3b76851ce3dead90
Juliherms de Lima Vasconcelos <jlv@CESAR-LDAP.INTRANET>
2020-12-09 21:57:48 -0300

    example to implement post method

--- src/main/java/com/github/juliherms/resource/UserResource.java
+++ src/main/java/com/github/juliherms/resource/UserResource.java
@@ -3,10 +3,14 @@ package com.github.juliherms.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.github.juliherms.model.User;
 
@@ -37,4 +41,14 @@ public class UserResource {
 		initUsers();
 		return users;
 	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response inserir(User user) {
+
+		this.users.add(user);
+		return Response.status(Status.CREATED).build();
+
+	}
+
 }
